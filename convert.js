c68a1ea10ab761f2c9b490ba52a5ace51a048033
Samuel Costa <samuel.carvalho@picpay.com>
2022-06-23 11:46:23 -0300

    Atualização

---
+++ convert.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+const args = process.argv.slice(2);
+
+args.forEach((fileName) => readFile(fileName));
+
+function readFile(fileName) {
+    fs.readFile(`./${fileName}`, 'utf-8', function (err, data) {
+        if (err) throw err;
+        whiteLogFile(fileName, data);
+    });
+}
+
+function whiteLogFile(fileName, data) {
+    const convert = String(data)
+        .replace(/=\\\\\\"/g, "='")
+        .replace(/\\\\\\", /g, "', ")
+        .replace(/\\\\\\"\\",/g, "'\",")
+        .replace(/\\\\\\"/g, '"')
+        .replace(/\\\"/g, '"')
+        .replace(/\\/g, '')
+        .replace(/"{/g, '{')
+        .replace(/}"/g, '}');
+
+    const convertPretty = JSON.stringify(JSON.parse(convert), null, 4);
+
+    fs.writeFile(`./${fileName.replace(/.json/g, '')}-log-${Date.now()}.json`, convertPretty, { enconding: 'utf-8', flag: 'a' }, function (err) {
+        if (err) throw err;
+        console.log(`File ${fileName} convertido para json formatável.`);
+    });
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+### Conversor de logs json para uma formatação mais legivel para investigação e análise.
+
+
+- Necessário instalação do node na máquina.
+- Cole os arquivos json no mesmo direito do arquivo e rode o comando.
+
+        node convert.js filename.json
+
+- É possível passar mais de um arquivo
+
+        node convert.js filename-1.json filename-2.json ...
